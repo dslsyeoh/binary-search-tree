041e8872daf4c95434aee9f6bb02d7a399ba5a9d
dslsyeoh <syeoh91x@gmail.com>
2019-08-21 00:47:55 +0800

    rename class and add bst demo

---
+++ src/com/dsl/bst/BSTDemo.java
@@ -0,0 +1,13 @@
+package com.dsl.bst;
+
+public class BSTDemo
+{
+    public static void main(String[] args)
+    {
+        IntegerBST iBST = new IntegerBST();
+        iBST.demo();
+
+        StringBST sBST = new StringBST();
+        sBST.demo();
+    }
+}
