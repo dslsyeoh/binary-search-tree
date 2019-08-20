01b4811b774eb3e73952bace017a838b24eb6bf3
dslsyeoh <syeoh91x@gmail.com>
2019-08-21 01:03:39 +0800

    minor update

--- src/com/dsl/bst/BSTDemo.java
+++ src/com/dsl/bst/BSTDemo.java
@@ -9,10 +9,7 @@ public class BSTDemo
 {
     public static void main(String[] args)
     {
-        IntegerBST iBST = new IntegerBST();
-        iBST.demo();
-
-        StringBST sBST = new StringBST();
-        sBST.demo();
+        new IntegerBST().demo();
+        new StringBST().demo();
     }
 }
