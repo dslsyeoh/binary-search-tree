258e66ce72007558e48402de485aba4a123adcb3
dslsyeoh <syeoh91x@gmail.com>
2019-08-21 00:41:46 +0800

    add bst for string

---
+++ src/com/dsl/bst/StringBST.java
@@ -0,0 +1,26 @@
+package com.dsl.bst;
+
+import java.util.Arrays;
+import java.util.List;
+
+class StringBST extends GenericBST<String>
+{
+    StringBST()
+    {
+        super(String::equals, (source, target) -> source.compareToIgnoreCase(target) < 0, (source, target) -> source.compareToIgnoreCase(target) >= 0);
+    }
+
+    @Override
+    public void demo()
+    {
+        System.out.println("\n========================================= BEGIN BST FOR STRING DEMO =========================================");
+        // construct binary search tree
+        List<String> bst = Arrays.asList("this", "is", "demo", "binary", "search", "tree", "in", "string", "throne", "trex");
+        constructBST(bst);
+
+        // print initial binary search tree
+        print();
+
+        System.out.println("=========================================== END BST FOR STRING DEMO ===========================================\n");
+    }
+}
