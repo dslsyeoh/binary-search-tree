5ed1ed4a151b398ee247b6d698f751ea548603ba
dslsyeoh <syeoh91x@gmail.com>
2019-08-24 00:28:19 +0800

    update log message

--- src/com/dsl/bst/IntegerBST.java
+++ src/com/dsl/bst/IntegerBST.java
@@ -24,25 +24,25 @@ class IntegerBST extends GenericBST<Integer>
         CustomLogger.log(String.format("Construct %s into BST", bst.toString()), () -> constructBST(bst));
 
 //        // print initial binary search tree
-        CustomLogger.log("Inorder traversal of binary tree", this::printInorder);
-        CustomLogger.log("Preorder traversal of binary tree after insert", this::printPreOrder);
-        CustomLogger.log("Postorder traversal of binary tree after insert", this::printPostOrder);
+        CustomLogger.log("Inorder traversal of binary search tree", this::printInorder);
+        CustomLogger.log("Preorder traversal of binary search tree", this::printPreOrder);
+        CustomLogger.log("Postorder traversal of binary search tree", this::printPostOrder);
 
         // update node in binary search tree
         CustomLogger.log("update 70 to 72 in binary search tree", () -> update(70, 72));
 
         // print binary search tree after update
-        CustomLogger.log("Inorder traversal of binary tree after update", this::printInorder);
-        CustomLogger.log("Preorder traversal of binary tree after update", this::printPreOrder);
-        CustomLogger.log("Postorder traversal of binary tree after update", this::printPostOrder);
+        CustomLogger.log("Inorder traversal of binary search tree after update", this::printInorder);
+        CustomLogger.log("Preorder traversal of binary search tree after update", this::printPreOrder);
+        CustomLogger.log("Postorder traversal of binary search tree after update", this::printPostOrder);
 
         // insert node into binary search tree
         CustomLogger.log("insert 73 into binary search tree", () -> insert(73));
 
         // print binary search tree after insert
-        CustomLogger.log("Inorder traversal of binary tree after insert", this::printInorder);
-        CustomLogger.log("Preorder traversal of binary tree after insert", this::printPreOrder);
-        CustomLogger.log("Postorder traversal of binary tree after insert", this::printPostOrder);
+        CustomLogger.log("Inorder traversal of binary search tree after insert", this::printInorder);
+        CustomLogger.log("Preorder traversal of binary search tree after insert", this::printPreOrder);
+        CustomLogger.log("Postorder traversal of binary search tree after insert", this::printPostOrder);
 
         // initialize inputs to validate node in binary search tree
         List<Integer> validateNodes = Arrays.asList(11, 60, 20, 21, 50, 82);
@@ -63,16 +63,16 @@ class IntegerBST extends GenericBST<Integer>
         List<Integer> deleteNodes = Arrays.asList(11, 60);
 
         // delete nodes in binary search tree
-        CustomLogger.log("delete nodes in binary search tree", () -> delete(deleteNodes));
+        CustomLogger.log(String.format("delete %s in binary search tree", deleteNodes.toString()), () -> delete(validateNodes2));
 
         // print binary search tree after delete
-        CustomLogger.log("Inorder traversal of binary tree after delete", this::printInorder);
-        CustomLogger.log("Preorder traversal of binary tree after delete", this::printPreOrder);
-        CustomLogger.log("Postorder traversal of binary tree after delete", this::printPostOrder);
+        CustomLogger.log("Inorder traversal of binary search tree after delete", this::printInorder);
+        CustomLogger.log("Preorder traversal of binary search tree after delete", this::printPreOrder);
+        CustomLogger.log("Postorder traversal of binary search tree after delete", this::printPostOrder);
 
         // print binary search tree from specific node
-        CustomLogger.log("Inorder traversal of binary tree", () -> printInorder(12));
-        CustomLogger.log("Preorder traversal of binary tree", () -> printPreOrder(12));
-        CustomLogger.log("Postorder traversal of binary tree", () -> printPostOrder(12));
+        CustomLogger.log("Inorder traversal of 12 in binary search tree", () -> printInorder(12));
+        CustomLogger.log("Preorder traversal of 12 in binary search tree", () -> printPreOrder(12));
+        CustomLogger.log("Postorder traversal of 12 in binary search tree", () -> printPostOrder(12));
     }
 }
